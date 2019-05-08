cd53a16281e2dec508e250edb20b23bfb7d1857e
Audrey Agyemang <audreyagyemang310@gmail.com>
2019-05-08 06:33:34 -0400

    fixed port 8080

--- server.js
+++ server.js
@@ -2,39 +2,22 @@ var express = require("express");
 var bodyParser = require("body-parser");
 
 var app = express();
+require("../FriendFinder/app/routing/apiRoute")(app);
+require("../FriendFinder/app/routing/htmlRoute")(app);
+
 
 // Set the port of our application
 // process.env.PORT lets the port be set by Heroku
 var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-var exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
 app.listen(PORT, function(){
 
   console.log("connected to " + PORT);
 })
 
-// var mysql = require("mysql");
 
-// var connection = mysql.createConnection({
-//   host: "localhost",
-//   port: 3306,
-//   user: "root",
-//   password: "root",
-//   database: "friendfinderDB"
-// });
-
-// connection.connect(function(err) {
-//   if (err) {
-//     console.error("error connecting: " + err.stack);
-//     return;
-//   }
-
-//   console.log("connected as id " + connection.threadId);
-// });
